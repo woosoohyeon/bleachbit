b1e35030d2ce94b6a27ed723c666eaa6244d76d6
woosoohyeon <42229732+woosoohyeon@users.noreply.github.com>
2018-11-24 03:36:36 +0900

    Update Windows.py

--- bleachbit/Windows.py
+++ bleachbit/Windows.py
@@ -50,6 +50,7 @@ import sys
 
 from decimal import Decimal
 
+#win32가 sys.platform일때 import
 if 'win32' == sys.platform:
     import _winreg
     import pywintypes
@@ -67,7 +68,7 @@ if 'win32' == sys.platform:
 
 logger = logging.getLogger(__name__)
 
-
+#사용자에게 단일 파일을 선택하도록 요청하고 전체 경로를 반환하는 함수
 def browse_file(_, title):
     """Ask the user to select a single file.  Return full path"""
     try:
@@ -76,6 +77,7 @@ def browse_file(_, title):
                                         | win32con.OFN_FILEMUSTEXIST
                                         | win32con.OFN_HIDEREADONLY,
                                         Title=title)
+     #예외처리 
     except pywintypes.error as e:
         logger = logging.getLogger(__name__)
         if 0 == e.winerror:
@@ -83,12 +85,14 @@ def browse_file(_, title):
         else:
             logger.exception('exception in browse_file()')
         return None
+    #경로반환
     return ret[0]
 
-
+#사용자에게 파일 선택을 요청하고, 전체 경로를 반환하는 함수 
 def browse_files(_, title):
     """Ask the user to select files.  Return full paths"""
     try:
+        ## File 매개 변수는 버퍼 길이를 늘리는 해킹이다.
         # The File parameter is a hack to increase the buffer length.
         ret = win32gui.GetOpenFileNameW(None,
                                         File = '\x00' * 10240,
@@ -97,15 +101,19 @@ def browse_files(_, title):
                                         | win32con.OFN_FILEMUSTEXIST
                                         | win32con.OFN_HIDEREADONLY,
                                         Title=title)
+     #예외처리 
     except pywintypes.error as e:
         if 0 == e.winerror:
             logger.debug('browse_files(): user cancelled')
         else:
             logger.exception('exception in browse_files()')
         return None
+    #경로에 \x00을 붙이고 _split로 초기화
     _split = ret[0].split('\x00')
+    # _split의 길이가 1이면 (파일의 이름이 하나)
     if 1 == len(_split):
         # only one filename
+        # _split리턴
         return _split
     pathnames = []
     dirname = _split[0]
@@ -113,9 +121,10 @@ def browse_files(_, title):
         pathnames.append(os.path.join(dirname, fname))
     return pathnames
 
-
+#사용자에게 폴더를 선택하도록 요청하고 전체 경로 반환하는 함수
 def browse_folder(hwnd, title):
     """Ask the user to select a folder.  Return full path."""
+    #사용자가 폴더를 선택하면 전체 경로를 반환하고 아니면 none을 반환한다.
     pidl = shell.SHBrowseForFolder(hwnd, None, title)[0]
     if pidl is None:
         # user cancelled
@@ -123,7 +132,7 @@ def browse_folder(hwnd, title):
     fullpath = shell.SHGetPathFromIDList(pidl)
     return fullpath
 
-
+#CleanerML 및 Winapp2.ini에서 사용할 수 있도록 CSIDL에서 환경 변수를 정의하는 함수
 def csidl_to_environ(varname, csidl):
     """Define an environment variable from a CSIDL for use in CleanerML and Winapp2.ini"""
     try:
@@ -134,16 +143,18 @@ def csidl_to_environ(varname, csidl):
     # there is exception handling in set_environ()
     set_environ(varname, sppath)
 
-
+#현재 사용중인 파일 삭제. 
 def delete_locked_file(pathname):
     """Delete a file that is currently in use"""
+    #os의 경로가 존재한다면 삭제한다.
     if os.path.exists(pathname):
         MOVEFILE_DELAY_UNTIL_REBOOT = 4
         if 0 == windll.kernel32.MoveFileExW(pathname, None, MOVEFILE_DELAY_UNTIL_REBOOT):
             from ctypes import WinError
             raise WinError()
 
-
+#레지스트리 키 하에서 이름 붙여진 값을 삭제하고 reference가 
+#발견되었는지의 여부를 boolean으로 반환하는 함수 
 def delete_registry_value(key, value_name, really_delete):
     """Delete named value under the registry key.
     Return boolean indicating whether reference found and
@@ -172,25 +183,29 @@ def delete_registry_value(key, value_name, really_delete):
         return True
     raise RuntimeError('Unknown error in delete_registry_value')
 
-
+#값 및 sub-key을 포함한 레지스트리 키를 삭제하는 함수 
 def delete_registry_key(parent_key, really_delete):
     """Delete registry key including any values and sub-keys.
     Return boolean whether found and success.  If really
     delete is False (meaning preview), just check whether
     the key exists."""
+    #parent_key초기화 
     parent_key = str(parent_key)  # Unicode to byte string
     (hive, parent_sub_key) = split_registry_key(parent_key)
     hkey = None
     try:
         hkey = _winreg.OpenKey(hive, parent_sub_key)
     except WindowsError as e:
+        #winerror가 2개이면 false반환 
         if e.winerror == 2:
             # 2 = 'file not found' happens when key does not exist
             return False
     if not really_delete:
+        #키 값을 발견하면 return ture
         return True
     if not hkey:
         # key not found
+        #키 값이 없으면 return false 
         return False
     keys_size = _winreg.QueryInfoKey(hkey)[0]
     child_keys = []
@@ -201,9 +216,10 @@ def delete_registry_key(parent_key, really_delete):
     _winreg.DeleteKey(hive, parent_sub_key)
     return True
 
-
+#windows 업데이트 파일을 삭제하는 것에 대한 command를 반환하는 함수
 def delete_updates():
     """Returns commands for deleting Windows Updates files"""
+    #dir에 위치를 추가한다. 
     windir = bleachbit.expandvars('$windir')
     dirs = glob.glob(os.path.join(windir, '$NtUninstallKB*'))
     dirs += [bleachbit.expandvars('$windir\\SoftwareDistribution\\Download')]
@@ -234,7 +250,7 @@ def delete_updates():
     if wu_running:
         yield Command.Function(None, wu_service, " ".join(args))
 
-
+#레지스트리 키가 존재하는지 탐색하는 함수 
 def detect_registry_key(parent_key):
     """Detect whether registry key exists"""
     parent_key = str(parent_key)  # Unicode to byte string
@@ -251,12 +267,14 @@ def detect_registry_key(parent_key):
         return False
     return True
 
-
+#windows vista 이상에서는 관리자 권한을 가져온다. 
+#성공하면 True를 반환(원래 process) 실패하면 False를 반환하는 함수 
 def elevate_privileges():
     """On Windows Vista and later, try to get administrator
     privileges.  If successful, return True (so original process
     can exit).  If failed or not applicable, return False."""
 
+    #parse_windows_build()가 6개 이하이면 false반환 
     if parse_windows_build() < 6:
         # Windows XP does not have the UAC.
         # Vista is the first version Windows that has the UAC.
@@ -312,10 +330,11 @@ def elevate_privileges():
 
     return False
 
-
+#휴지통을 비우거나 파일 크기를 미리보는 함수 
 def empty_recycle_bin(path, really_delete):
     """Empty the recycle bin or preview its size.
 
+    #만약 휴지통이 비어있으면 오류를 피하기 위해 다시 비워지지 않음. 
     If the recycle bin is empty, it is not emptied again to avoid an error.
 
     Keyword arguments:
@@ -323,14 +342,16 @@ def empty_recycle_bin(path, really_delete):
     really_delete -- If True, then delete.  If False, then just preview.
     """
     (bytes_used, num_files) = shell.SHQueryRecycleBin(path)
+    #삭제파일 혹은 파일의 개수가 0초과이면 
     if really_delete and num_files > 0:
         # Trying to delete an empty Recycle Bin on Vista/7 causes a
         # 'catastrophic failure'
         flags = shellcon.SHERB_NOSOUND | shellcon.SHERB_NOCONFIRMATION | shellcon.SHERB_NOPROGRESSUI
         shell.SHEmptyRecycleBin(None, path, flags)
+    #bytes_used을 리턴 
     return bytes_used
 
-
+#사용자 시작 폴더에 있는 bleachbit 바로가기 폴더 경로를 반환하는 함수
 def get_autostart_path():
     """Return the path of the BleachBit shortcut in the user's startup folder"""
     try:
@@ -353,7 +374,7 @@ def get_autostart_path():
             startupdir = bleachbit.expandvars('$APPDATA\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup')
     return os.path.join(startupdir, 'bleachbit.lnk')
 
-
+#클리보드의 유니코드 경로 이름 반환하는 함수 
 def get_clipboard_paths():
     """Return a tuple of Unicode pathnames from the clipboard"""
     import win32clipboard
@@ -365,10 +386,14 @@ def get_clipboard_paths():
         pass
     finally:
         win32clipboard.CloseClipboard()
+     #경로 이름을 반환한다.
     return path_list
 
+#고정 드라이브 알려주는 함수 
 def get_fixed_drives():
     """Yield each fixed drive"""
+    
+    #GetLogicalDriveStrings()에서 고정드라이브문자열에서 \x00을 자른다. 
     for drive in win32api.GetLogicalDriveStrings().split('\x00'):
         if win32file.GetDriveType(drive) == win32file.DRIVE_FIXED:
             # Microsoft Office 2010 Starter creates a virtual drive that
@@ -378,10 +403,11 @@ def get_fixed_drives():
             if os.path.isdir(drive):
                 yield unicode(drive)
 
-
+#폴더 ID를 기준으로 폴더 경로 반환하는 함수 
 def get_known_folder_path(folder_name):
     """Return the path of a folder by its Folder ID
-
+    
+    #Windows vista, 서버 2008 이상 필요함.
     Requires Windows Vista, Server 2008, or later
 
     Based on the code Michael Kropat (mkropat) from
@@ -440,13 +466,16 @@ def get_known_folder_path(folder_name):
     _CoTaskMemFree(pPath)
     return path
 
-
+#휴지통에 있는 파일 목록 넘겨주는 함수 
 def get_recycle_bin():
     """Yield a list of files in the recycle bin"""
+    #pidl초기화 
     pidl = shell.SHGetSpecialFolderLocation(0, shellcon.CSIDL_BITBUCKET)
+    #desktop변수 초기화 
     desktop = shell.SHGetDesktopFolder()
     h = desktop.BindToObject(pidl, None, shell.IID_IShellFolder)
     for item in h:
+        #h에 경로를 넣는다.
         path = h.GetDisplayNameOf(item, shellcon.SHGDN_FORPARSING)
         if os.path.isdir(path):
             for child in FileUtilities.children_in_directory(path, True):
@@ -455,14 +484,14 @@ def get_recycle_bin():
         else:
             yield path
 
-
+#10.0과 같이 Windows 기본 버전과 보조 버전을 가져오는 함수 
 def get_windows_version():
     """Get the Windows major and minor version in a decimal like 10.0"""
     v = win32api.GetVersionEx(0)
     vstr = '%d.%d' % (v[0], v[1])
     return Decimal(vstr)
 
-
+#irefox.exe와 같이 실행중인 프로세스의 boolean을 return한는 함수 
 def is_process_running(name):
     """Return boolean whether process (like firefox.exe) is running"""
 
@@ -473,7 +502,8 @@ def is_process_running(name):
         # https://github.com/giampaolo/psutil/issues/348
         return is_process_running_win32(name)
 
-
+#firefox.exe와 같이 실행중인 프로세스의 boolean을 return한는 함수 
+#(64비트 Windows에서는 작동 X).
 def is_process_running_win32(name):
     """Return boolean whether process (like firefox.exe) is running
 
@@ -520,10 +550,11 @@ def is_process_running_win32(name):
 
     return False
 
-
+#firefox.exe와 같이 실행중인 프로세스의 boolean을 return하는 함수 
 def is_process_running_psutil(name):
     """Return boolean whether process (like firefox.exe) is running
 
+    Windows XP에서 ImportError 표시.
     Works on Windows Vista or later, but on Windows XP gives an ImportError
     """
 
@@ -537,13 +568,13 @@ def is_process_running_psutil(name):
             pass
     return False
 
-
-def move_to_recycle_bin(path):
+# 휴지통 안으로 경로 이동
+def move_to_recycle_bin(p#ath):
     """Move 'path' into recycle bin"""
     shell.SHFileOperation(
         (0, shellcon.FO_DELETE, path, None, shellcon.FOF_ALLOWUNDO | shellcon.FOF_NOCONFIRMATION))
 
-
+#빌드 문자열을 1.2.3 또는 1.2와 같이 구문 분석하는 함수 
 def parse_windows_build(build=None):
     """
     Parse build string like 1.2.3 or 1.2 to numeric,
@@ -554,7 +585,7 @@ def parse_windows_build(build=None):
         return get_windows_version()
     return Decimal('.'.join(build.split('.')[0:2]))
 
-
+#‘path’가 네트워크 드라이브에 있는지 확인하는 함수 
 def path_on_network(path):
     """Check whether 'path' is on a network drive"""
     if len(os.path.splitunc(path)[0]) > 0:
@@ -562,15 +593,17 @@ def path_on_network(path):
     drive = os.path.splitdrive(path)[0] + '\\'
     return win32file.GetDriveType(drive) == win32file.DRIVE_REMOTE
 
-
+#Windows 업데이트를 shell에 알리는 함수
 def shell_change_notify():
     """Notify the Windows shell of update.
-
+    
+    Window_Thomer.xml에 사용된다.. 
     Used in windows_explorer.xml."""
     shell.SHChangeNotify(shellcon.SHCNE_ASSOCCHANGED, shellcon.SHCNF_IDLIST,
                          None, None)
     return 0
-
+                        
+#CleanerML 및 Winapp2.ini에서 사용할 환경 변수를 정의하는 함수
 def set_environ(varname, path):
     """Define an environment variable for use in CleanerML and Winapp2.ini"""
     if not path:
@@ -589,7 +622,7 @@ def set_environ(varname, path):
     except:
         logger.exception('set_environ(%s, %s): exception when setting environment variable', varname, path)
 
-
+#CleanerML 및 Winapp2.ini에서 사용할 추가 환경 변수 정의.
 def setup_environment():
     """Define any extra environment variables for use in CleanerML and Winapp2.ini"""
     csidl_to_environ('commonappdata', shellcon.CSIDL_COMMON_APPDATA)
@@ -613,7 +646,7 @@ def setup_environment():
     # cmd.exe .
     set_environ('cd', os.getcwd())
 
-
+#HKLM 소프트웨어 같은 키가 튜플로 분할되어 있는 경우 내부적으로 사용하는 함수.
 def split_registry_key(full_key):
     r"""Given a key like HKLM\Software split into tuple (hive, key).
     Used internally."""
@@ -628,10 +661,11 @@ def split_registry_key(full_key):
         raise RuntimeError("Invalid Windows registry hive '%s'" % k1)
     return hive_map[k1], k2
 
-
+#사용 가능한 경우 바로가기를 만들어 컴퓨터로 응용 프로그램을 시작하는 함수
 def start_with_computer(enabled):
     """If enabled, create shortcut to start application with computer.
     If disabled, then delete the shortcut."""
+     #함수 사용하지 않도록 설정된 경우 바로가기를 삭제
     autostart_path = get_autostart_path()
     if not enabled:
         if os.path.lexists(autostart_path):
@@ -650,7 +684,7 @@ def start_with_computer(enabled):
     #     Common.bleachbit_exe_path, 'bleachbit.exe')
     # shortcut.save()
 
-
+#BleachBit이 컴퓨터로 시작할지 여부를 boolean으로 반환하는 함수
 def start_with_computer_check():
     """Return boolean whether BleachBit will start with the computer"""
     return os.path.lexists(get_autostart_path())
